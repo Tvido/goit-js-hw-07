793537035af25c88512cda6b37e35cf4ccadcd7f
Volodymyr Viter <mad.tvido@gmail.com>
2021-02-28 10:05:24 +0200

    Update task-7.js

---
+++ js/task-7.js
@@ -0,0 +1,18 @@
+// Задание 7
+// Напиши скрипт, который реагирует на изменение значения 
+// input#font - size - control(событие input) и изменяет инлайн 
+// - стиль span#text обновляя свойство font - size.
+// В результате при перетаскивании ползунка будет меняться размер текста.
+
+// <input id="font-size-control" type="range" />
+// <br />
+// <span id="text">Абракадабра!</span>
+
+const fontSizeControlRef = document.querySelector('#font-size-control');
+const textRef = document.querySelector('#text');
+
+function size (event) {
+    textRef.style.fontSize = `${event.currentTarget.value}px`;
+};
+
+fontSizeControlRef.addEventListener('input', size);
