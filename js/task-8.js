7cefbd40cadf7d2027317bf30512773bd9d1189a
Volodymyr Viter <mad.tvido@gmail.com>
2021-02-28 15:13:12 +0200

    Update task-8.js

--- js/task-8.js
+++ js/task-8.js
@@ -51,35 +51,84 @@
 // btnRender.addEventListener('click', handleIntroductionNumber);
 
 // #2
-const control = document.querySelector('#controls');
-const userInput =control.querySelector('input');
-const boxes = document.querySelector('#boxes');
-const renderBtn = control.querySelector('button[data-action="render"]');
-const destroyBtn = control.querySelector('button[data-action="destroy"]');
+// const control = document.querySelector('#controls');
+// const userInput =control.querySelector('input');
+// const boxes = document.querySelector('#boxes');
+// const renderBtn = control.querySelector('button[data-action="render"]');
+// const destroyBtn = control.querySelector('button[data-action="destroy"]');
 
-const createBoxes = (amount) => {
-    destroyBoxes();
-    let size = 30;
-    const itemsList = [];
+// const createBoxes = (amount) => {
+//     destroyBoxes();
+//     let size = 30;
+//     const itemsList = [];
        
-    for (let i = 0; i < amount; i += 1){
-        const colorRandom = Math.floor(Math.random()*16777215).toString(16);
-        const item = document.createElement('div');
-        item.style.width = `${size}px`;
-        item.style.height = `${size}px`;
-        item.style.marginBottom = '10px';
-        item.style.backgroundColor = `#${colorRandom}`;
-        itemsList.push(item);
-        size += 10;
-    };
-    boxes.prepend(...itemsList);
-}
+//     for (let i = 0; i < amount; i += 1){
+//         const colorRandom = Math.floor(Math.random()*16777215).toString(16);
+//         const item = document.createElement('div');
+//         item.style.width = `${size}px`;
+//         item.style.height = `${size}px`;
+//         item.style.marginBottom = '10px';
+//         item.style.backgroundColor = `#${colorRandom}`;
+//         itemsList.push(item);
+//         size += 10;
+//     };
+//     boxes.prepend(...itemsList);
+// }
 
-const destroyBoxes = () => {
-    while (boxes.firstChild) {
-        boxes.removeChild(boxes.lastChild);
-    };
+// const destroyBoxes = () => {
+//     while (boxes.firstChild) {
+//         boxes.removeChild(boxes.lastChild);
+//     };
+// };
+
+// renderBtn.addEventListener('click', () => createBoxes(userInput.value));
+// destroyBtn.addEventListener('click', () => destroyBoxes());
+
+// #3
+const btnBox = document.querySelector("#controls");
+const btnDestroy = btnBox.lastElementChild;
+const btnRender = btnDestroy.previousElementSibling;
+const boxesEl = document.querySelector("#boxes");
+const numberEl = btnBox.firstElementChild;
+
+function handleClearOutput(){
+  boxesEl.innerHTML = "";
 };
 
-renderBtn.addEventListener('click', () => createBoxes(userInput.value));
-destroyBtn.addEventListener('click', () => destroyBoxes());
+function handleIntroductionNumber(){
+  
+    if(boxesEl.childNodes.length != 0){
+      handleClearOutput();
+      handleIntroductionNumber();
+    return
+  }
+
+  const numberOfInputs = numberEl.value;
+  const result = [];
+
+  for (let i = 0; i < numberOfInputs; i += 1) {
+    const divs = document.createElement("div");
+    
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+    const colorNumvber =
+      "rgb(" +
+      `${getRandomInt(0, 1000)}` +
+      "," +
+      `${getRandomInt(0, 1000)}` +
+      "," +
+      `${getRandomInt(0, 1000)}` +
+      ")";
+    const blockSize = 30 + i * 10;
+    divs.style.cssText = `height: ${blockSize}px; width: ${blockSize}px; background-color: ${colorNumvber}`;
+
+    result.push(divs);
+  }
+  boxesEl.append(...result);
+
+};
+
+
+btnDestroy.addEventListener("click", handleClearOutput);
+btnRender.addEventListener("click", handleIntroductionNumber);
