ff5d6e88fb108cc3b32ed64ded6f4355efe87166
Volodymyr Viter <mad.tvido@gmail.com>
2021-02-28 21:08:03 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,7 +3,28 @@
 // <input type="text" placeholder="Ваше имя?" id="name-input" />
 // <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
 
+// #1
+// const refs = {
+//   input: document.querySelector('#name-input'),
+//   nameLabel: document.querySelector('#name-output'),
+// };
 
+// refs.input.addEventListener('input', onInputChange);
+
+// function onInputChange(event) {
+//   refs.nameLabel.textContent = event.currentTarget.value;
+// };
+
+// #2
+// const userInput = document.querySelector('#name-input');
+// const greetOutput = document.querySelector('#name-output');
+
+// const greeting = (inputEvent) => {
+//     greetOutput.textContent = inputEvent.target.value ? inputEvent.target.value : 'незнакомец';    
+// }
+// userInput.addEventListener('input', greeting);
+
+// #3
 const refs = {
   input: document.querySelector('#name-input'),
   nameLabel: document.querySelector('#name-output'),
@@ -12,5 +33,9 @@ const refs = {
 refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  refs.nameLabel.textContent = event.currentTarget.value;
+  if (refs.input.value === "") {
+    refs.nameLabel.textContent = "незнакомец";
+  } else {
+    refs.nameLabel.textContent = event.currentTarget.value;
+  };
 };
